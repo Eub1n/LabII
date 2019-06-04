2d695401b2667670675dd66ce208a89781a127b2
Weber Marques <webermarques9@gmail.com>
2019-06-03 21:09:24 -0300

    Add files via upload

--- src/questao2/Main.java
+++ src/questao2/Main.java
@@ -1,14 +1,27 @@
 package questao2;
-
+import java.util.Random;
 public class Main {
+
+   public static void preencherVetores(int[] vetorA, int[] vetorB, int[] vetorC, int n) {
+		Random r = new Random();
+		for (int i = 0; i < n; i++) {
+			vetorA[i] = i + 1;
+			vetorB[i] = vetorB.length - (i);
+			vetorC[i] = r.nextInt(1000);
+   	}
+   }
+
 	
 	public static void main(String[] args) {
 		int[] vetorA = new int[100000];
 		int[] vetorB = new int[100000];
 		int[] vetorC = new int[100000];
-      TypesOfSorts.preencherVetores(vetorA, vetorB, vetorC, 100000);
-		
-     /* Timer timerBubbleA = new Timer();
+     preencherVetores(vetorA, vetorB, vetorC, 100000);
+      
+      
+      
+		//Bubble normal
+      Timer timerBubbleA = new Timer();
 		TypesOfSorts.BBSortClassico(vetorA);
 		String tBubbleA = timerBubbleA.getTotalTime();
       
@@ -20,6 +33,7 @@ public class Main {
 		TypesOfSorts.BBSortClassico(vetorC);
 		String tBubbleC = timerBubbleC.getTotalTime();
       
+      //Bubble adaptado1
       Timer timerBubbleAdapt1A = new Timer();
 		TypesOfSorts.BBSortAdapt1(vetorA);
 		String tBubbleAdapt1A = timerBubbleAdapt1A.getTotalTime();
@@ -31,56 +45,131 @@ public class Main {
       Timer timerBubbleAdapt1C = new Timer();
 		TypesOfSorts.BBSortAdapt1(vetorC);
 		String tBubbleAdapt1C = timerBubbleAdapt1C.getTotalTime();
-      */
-      Timer timerBubbleAdapt1A = new Timer();
+      
+      //Bubble adaptado2
+      Timer timerBubbleAdapt2A = new Timer();
 		TypesOfSorts.BBSortAdapt2(vetorA);
-		String tBubbleAdapt1A = timerBubbleAdapt1A.getTotalTime();
+		String tBubbleAdapt2A = timerBubbleAdapt2A.getTotalTime();
       
-      Timer timerBubbleAdapt1B = new Timer();
+      Timer timerBubbleAdapt2B = new Timer();
 		TypesOfSorts.BBSortAdapt2(vetorB);
-		String tBubbleAdapt1B = timerBubbleAdapt1B.getTotalTime();
+		String tBubbleAdapt2B = timerBubbleAdapt2B.getTotalTime();
       
-      Timer timerBubbleAdapt1C = new Timer();
+      Timer timerBubbleAdapt2C = new Timer();
 		TypesOfSorts.BBSortAdapt2(vetorC);
-		String tBubbleAdapt1C = timerBubbleAdapt1C.getTotalTime();
+		String tBubbleAdapt2C = timerBubbleAdapt2C.getTotalTime();
+     
+      //Insercao
+      Timer timerInsercaoA = new Timer();
+		TypesOfSorts.Insercao(vetorA);
+		String tInsercaoA = timerInsercaoA.getTotalTime();
+      
+      Timer timerInsercaoB = new Timer();
+		TypesOfSorts.Insercao(vetorB);
+		String tInsercaoB = timerInsercaoB.getTotalTime();
+      
+      Timer timerInsercaoC = new Timer();
+		TypesOfSorts.Insercao(vetorC);
+		String tInsercaoC = timerInsercaoC.getTotalTime();
+  
+      
+      //Selecao
+      Timer timerSelecaoA = new Timer();
+		TypesOfSorts.Selecao(vetorA);
+		String tSelecaoA = timerSelecaoA.getTotalTime();
+      
+      Timer timerSelecaoB = new Timer();
+		TypesOfSorts.Selecao(vetorB);
+		String tSelecaoB = timerSelecaoB.getTotalTime();
+      
+      Timer timerSelecaoC = new Timer();
+		TypesOfSorts.Selecao(vetorC);
+		String tSelecaoC = timerSelecaoC.getTotalTime();
 
-		
-		// Imprime os resultados
-		/*System.out.println("Tempo Bubble:");
+      //Merge
+      Timer timerMergeA = new Timer();
+		TypesOfSorts.chamaMergeSort(vetorA);
+		String tMergeA = timerMergeA.getTotalTime();
+      
+      Timer timerMergeB = new Timer();
+		TypesOfSorts.chamaMergeSort(vetorB);
+		String tMergeB = timerMergeB.getTotalTime();
+      
+      Timer timerMergeC = new Timer();
+		TypesOfSorts.chamaMergeSort(vetorC);
+		String tMergeC = timerMergeC.getTotalTime();
+
+		//Shell
+      Timer timerShellA = new Timer();
+		TypesOfSorts.ShellSort(vetorA);
+		String tShellA = timerShellA.getTotalTime();
+      
+      Timer timerShellB = new Timer();
+		TypesOfSorts.ShellSort(vetorB);
+		String tShellB = timerShellB.getTotalTime();
+      
+      Timer timerShellC = new Timer();
+		TypesOfSorts.ShellSort(vetorC);
+		String tShellC = timerShellC.getTotalTime();
+      
+      /*
+      //Quick
+      Timer timerQuickA = new Timer();
+		TypesOfSorts.chamaQuicksort(vetorA);
+		String tQuickA = timerQuickA.getTotalTime();
+      
+      Timer timerQuickB = new Timer();
+		TypesOfSorts.chamaQuicksort(vetorB);
+		String tQuickB = timerQuickB.getTotalTime();
+      
+      Timer timerQuickC = new Timer();
+		TypesOfSorts.chamaQuicksort(vetorC);
+		String tQuickC = timerQuickC.getTotalTime();
+*/
+      
+		// Imprime os resultados 
+      
+		System.out.println("Tempo Bubble:");
 		System.out.println("Crescente: " + tBubbleA);
 		System.out.println("Decrescente: " + tBubbleB);
 		System.out.println("Aleatorio: " + tBubbleC);
-		System.out.println();*/
+		System.out.println();
       System.out.println("Tempo BubbleAdapt1:");
 		System.out.println("Crescente: " + tBubbleAdapt1A);
 		System.out.println("Decrescente: " + tBubbleAdapt1B);
 		System.out.println("Aleatorio: " + tBubbleAdapt1C);
 		System.out.println();
-
-		/*System.out.println("Tempo Insert:");
-		System.out.println("Crescente: " + tempoInsertCres);
-		System.out.println("Decrescente: " + tempoInsertDecr);
-		System.out.println("Aleatorio: " + tempoInsertRand);
+      System.out.println("Tempo BubbleAdapt2:");
+		System.out.println("Crescente: " + tBubbleAdapt2A);
+		System.out.println("Decrescente: " + tBubbleAdapt2B);
+		System.out.println("Aleatorio: " + tBubbleAdapt2C);
 		System.out.println();
-		System.out.println("Tempo Selection:");
-		System.out.println("Crescente: " + tempoSelectionCres);
-		System.out.println("Decrescente: " + tempoSelectionDecr);
-		System.out.println("Aleatorio: " + tempoSelectionRand);
+      System.out.println("Tempo Insercao:");
+		System.out.println("Crescente: " + tInsercaoA);
+		System.out.println("Decrescente: " + tInsercaoB);
+		System.out.println("Aleatorio: " + tInsercaoC);
 		System.out.println();
-		System.out.println("TempoMerge:");
-		System.out.println("Crescente: " + tempoMergeCres);
-		System.out.println("Decrescente: " + tempoMergeDecr);
-		System.out.println("Aleatorio: " + tempoMergeRand);
+      System.out.println("Tempo Selecao:");
+		System.out.println("Crescente: " + tSelecaoA);
+		System.out.println("Decrescente: " + tSelecaoB);
+		System.out.println("Aleatorio: " + tSelecaoC);
+		System.out.println();  
+      System.out.println("Tempo Merge:");
+		System.out.println("Crescente: " + tMergeA);
+		System.out.println("Decrescente: " + tMergeB);
+		System.out.println("Aleatorio: " + tMergeC);
 		System.out.println();
-		System.out.println("Tempo Quick:");
-		System.out.println("Crescente: " + tempoQuickCres);
-		System.out.println("Decrescente: " + tempoQuickDecr);
-		System.out.println("Aleatorio: " + tempoQuickRand);
+      System.out.println("Tempo Shell:");
+		System.out.println("Crescente: " + tShellA);
+		System.out.println("Decrescente: " + tShellB);
+		System.out.println("Aleatorio: " + tShellC);
 		System.out.println();
-		System.out.println("Tempo Shell:");
-		System.out.println("Crescente: " + tempoShellCres);
-		System.out.println("Decrescente: " + tempoShellDecr);
-		System.out.println("Aleatorio: " + tempoShellRand);
-		System.out.println();*/
+/*
+      System.out.println("Tempo Quick:");
+		System.out.println("Crescente: " + tQuickA);
+		System.out.println("Decrescente: " + tQuickB);
+		System.out.println("Aleatorio: " + tQuickC);
+		System.out.println();
+*/
 	}
 }
--- src/questao2/TypesOfSorts.java
+++ src/questao2/TypesOfSorts.java
@@ -1,17 +1,7 @@
 package questao2;
-import java.util.Random;
+
 
 public final class TypesOfSorts {
-
-public static void preencherVetores(int[] vetorA, int[] vetorB, int[] vetorC, int n) {
-		Random r = new Random();
-		for (int i = 0; i < n; i++) {
-			vetorA[i] = i + 1;
-			vetorB[i] = vetorB.length - (i);
-			vetorC[i] = r.nextInt(1000);
-		}
-	}
-
 
 	public static void BBSortClassico(int[] vetor) {
 		int i, j, aux;
@@ -21,8 +11,8 @@ public static void preencherVetores(int[] vetorA, int[] vetorB, int[] vetorC, in
 					aux = vetor[j];
 					vetor[j] = vetor[j + 1];
 					vetor[j + 1] = aux; 
-				}
 	}
+}
 
 	public static void BBSortAdapt1(int[] vetor) {
 		int i, aux = 0;
@@ -34,9 +24,9 @@ public static void preencherVetores(int[] vetorA, int[] vetorB, int[] vetorC, in
 					vetor[i] = vetor[i + 1];
 					vetor[i + 1] = aux;
 					HouveTroca = true;
-				}
 		}
 	}
+}
 
 	public static void BBSortAdapt2(int[] vetor) {
 		int i, j, aux, cont;
@@ -47,10 +37,24 @@ public static void preencherVetores(int[] vetorA, int[] vetorB, int[] vetorC, in
 					aux = vetor[j];
 					vetor[j] = vetor[j + 1];
 					vetor[j + 1] = aux;
-				}
-			cont--;
 		}
+			cont--;
 	}
+}
+
+   public static void Insercao(int[] vetor) {
+		int i, j, v;
+		for (i = 1; i < vetor.length; i++) {
+			v = vetor[i];
+			j = i;
+			while ((j > 0) && (vetor[j - 1] > v)) {
+				vetor[j] = vetor[j - 1];
+				j--;
+		}
+			vetor[j] = v;
+	}
+}
+
 
 	public static void Selecao(int[] vetor) {
 		int i, j, min, aux;
@@ -62,85 +66,58 @@ public static void preencherVetores(int[] vetorA, int[] vetorB, int[] vetorC, in
 			aux = vetor[min];
 			vetor[min] = vetor[i];
 			vetor[i] = aux;
-		}
 	}
+}
+   public static void chamaMergeSort(int v[] ) {
+		MergeSort(v,100000);
+ }
 
-	public static void Insercao(int[] vetor) {
-		int i, j, v;
-		for (i = 1; i < vetor.length; i++) {
-			v = vetor[i];
-			j = i;
-			while ((j > 0) && (vetor[j - 1] > v)) {
-				vetor[j] = vetor[j - 1];
-				j--;
-			}
-			vetor[j] = v;
-		}
-	}
-
-	public static void Quicksort(int v[], int esquerda, int direita) {
-		int e = esquerda,d = direita,pivo = v[(e + d) % 2],troca;
-		while (e <= d) {
-			while (v[e] < pivo) {
-				e = e + 1;
-			}
-			while (v[d] > pivo) {
-				d = d - 1;
-			}
-			if (e <= d) {
-				troca = v[e];
-				v[e] = v[d];
-				v[d] = troca;
-				e = e + 1;
-				d = d - 1;
-			}
-		}
-		if (d > esquerda)
-			Quicksort(v, esquerda, d);
-		if (e < direita)
-			Quicksort(v, e, direita);
-	}
+	 public static void MergeSort(int[] a, int n) {
+       if (n < 2) {
+           return;
+       }
+       int mid = n / 2;
+       int[] l = new int[mid];
+       int[] r = new int[n - mid];
+    
+       for (int i = 0; i < mid; i++) {
+           l[i] = a[i];
+       }
+       for (int i = mid; i < n; i++) {
+           r[i - mid] = a[i];
+       }
+       MergeSort(l, mid);
+       MergeSort(r, n - mid);
+    
+       merge(a, l, r, mid, n - mid);
+   }
    
+   public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+     
+       int i = 0, j = 0, k = 0;
+       while (i < left && j < right) {
+           if (l[i] <= r[j]) {
+               a[k++] = l[i++];
+           }
+           else {
+               a[k++] = r[j++];
+           }
+       }
+       while (i < left) {
+           a[k++] = l[i++];
+       }
+       while (j < right) {
+           a[k++] = r[j++];
+       }
+}
 
 	
-	public static void Mergesort(int v[], int inicio, int fim) {
-		int meio;
-		if (inicio < fim) { 
-			meio = (inicio + fim) / 2; 
-			Mergesort(v, inicio, meio); 
-			Mergesort(v, meio + 1, fim); 
-			Merge(v, inicio, meio, fim); 
-		}
-	}
-   
-   public static void Merge(int v[], int inicio, int meio, int fim) {
-		int i, p1, p2;
-		int aux[] = new int[fim + 1];
-
-		p1 = inicio; 
-		p2 = meio + 1; 
-		i = inicio; 
-		while (p1 <= meio && p2 <= fim) { 
-			if (v[p1] <= v[p2])
-				aux[i++] = v[p1++]; 
-			else
-				aux[i++] = v[p2++];
-		}
-		while (p1 <= meio)
-			aux[i++] = v[p1++]; 
-		while (p2 <= fim)
-			aux[i++] = v[p2++];
-
-		for (i = inicio; i <= fim; i++)
-			v[i] = aux[i]; 
-	}
-	
-	public static void shellSort ( int [ ] v )
+	public static void ShellSort ( int [ ] v )
     {  
        int i , j , x = 1, value ;
        do {
           x = 3 * x + 1;
-       } while ( x < v.length );
+      } while ( x < v.length );
        do {
           x = x / 3;
           for ( i = x; i < v.length; i++) {
@@ -149,9 +126,38 @@ public static void preencherVetores(int[] vetorA, int[] vetorB, int[] vetorC, in
              while (j >= 0 && value < v [ j ]) {
                 v [ j + x ] = v [ j ];
                 j = j - x;
-             }
+            }
              v [ j + x ] = value;
-          }
+         }
     } while ( x > 1 );
    }
+   
+   public static void chamaQuicksort(int v[] ) {
+		Quicksort(v,0,100000);
+ }
+
+   
+   public static void Quicksort(int v[], int esquerda, int direita) {
+		int e = esquerda,d = direita,pivo = v[(e + d) % 2],troca;
+		while (e <= d) {
+			while (v[e] < pivo) {
+				e = e + 1;
+	}
+			while (v[d] > pivo) {
+				d = d - 1;
+	}
+			if (e <= d) {
+				troca = v[e];
+				v[e] = v[d];
+				v[d] = troca;
+				e = e + 1;
+				d = d - 1;
+		}
+	}
+		if (d > esquerda)
+			Quicksort(v, esquerda, d);
+		if (e < direita)
+			Quicksort(v, e, direita);
+}
+
 }
